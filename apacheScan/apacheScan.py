8cc0283120cd850918ade7732ab761ee0457a94d
mrpnkt <mr@mrpnkt.com>
2018-01-25 12:44:01 +0100

    Update apacheScan.py

--- apacheScan/apacheScan.py
+++ apacheScan/apacheScan.py
@@ -36,6 +36,7 @@ def checkVulns(banner, filename):
         print bcolors.SUCCESS + bcolors.BOLD + '\n[+]' + bcolors.ENDC + ' Found' + bcolors.FAIL + bcolors.BOLD, len(vulns), bcolors.ENDC + varVuln,
         print bcolors.SUCCESS + bcolors.BOLD + '\n[+]' + bcolors.ENDC + ' Apache ' + bcolors.NEUTRAL + bcolors.BOLD + banner + bcolors.ENDC + ' is vulnerable to the following:'
         print bcolors.FAIL + "".join(vulns)
+        print bcolors.NEUTRAL
     else:
         print bcolors.FAIL + bcolors.BOLD + '\n[-]' + bcolors.ENDC + ' I am sorry but we could\'t find any vulnerabilities in our database for ' + bcolors.NEUTRAL + bcolors.BOLD + 'Apache ' + banner + bcolors.ENDC + '.'
 
@@ -52,7 +53,7 @@ def main():
             print '[-] ' + filename + 'access denied.'
             exit(0)
         checkVulns(version, filename)
-        print bcolors.ENDC
+        print bcolors.NEUTRAL
     else:
         print '\nUsage:'
         print '\tpython apacheScan.py [version]'
